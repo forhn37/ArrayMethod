e9faf85e2e792872e90dcdd06f55e7ff5a6ede8e
kimhyun <forhn37@gmail.com>
2023-11-08 22:56:51 +0900

    8번 문제 완료

---
+++ ArrayMethod/array8.js
@@ -0,0 +1,8 @@
+var fruits = ["사과", "바나나", "오렌지"];
+var result = fruits.join(" ");
+
+console.log(result); // "사과 바나나 오렌지"
+
+
+// ! array.join(separator);
+// ! separator: 배열 요소를 연결할 때 사용할 문자열 구분자를 지정합니다. 이 구분자는 요소 사이에 삽입됩니다.
